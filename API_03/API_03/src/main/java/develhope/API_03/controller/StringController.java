5718c31d9c48b09b632a297bc1e366ef662b0abc
Giuseppe Corrao <josephcorr99@gmail.com>
2022-10-18 16:52:52 +0200

    second commit

--- API_03/API_03/src/main/java/develhope/API_03/controller/StringController.java
+++ API_03/API_03/src/main/java/develhope/API_03/controller/StringController.java
@@ -2,15 +2,15 @@ package develhope.API_03.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StringController {
 
     @GetMapping("/strings")
-    public String concat2String(String mandatory,String string) {
+    public String concat2String(@RequestParam String mandatory, String string) {
 
-        mandatory = "welcome ";
 
         if (string == null) {
             return mandatory;
